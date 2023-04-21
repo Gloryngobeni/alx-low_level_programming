1a236460573021b5af032042497f3e5374c6cb36
Glory Ngobeni <128898813+Gloryngobeni@users.noreply.github.com>
2023-04-21 03:09:00 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-glory
+
+#include <stdio.h>
+/**
+* main - Entry 
+*
+* Return: Always 0
+*/
+int main(void)
+{
+      printf("Size of a char: %i byte(s)\n",  sizeof(char));
+Repeat the same line but where you see char..u replace with an int, 
+Third line replace with...a long int, 
+Fourth line replace with...a long long int,
+Fifth line replace with...a float.
