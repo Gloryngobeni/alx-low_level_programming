1f0682e1cd380821f76bc9ad56f2f036d126ab37
Glory Ngobeni <128898813+Gloryngobeni@users.noreply.github.com>
2023-04-27 00:29:33 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
-glory
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
+
+
