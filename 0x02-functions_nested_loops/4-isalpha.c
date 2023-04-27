0777ab57297c2ca76bfb2c2c6522213f8dd4f8ce
Glory Ngobeni <128898813+Gloryngobeni@users.noreply.github.com>
2023-04-27 03:20:26 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,14 @@
-glory
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic order
+ *
+ * @c: alpabet to check for
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
+
