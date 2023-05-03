6af2c3228a6a9bec8ac1c48fd4c59c9e3e950672
Gloryngobeni <www.gloryngobeni@gmail.com>
2023-05-02 18:35:13 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char rev = s[o];
+	int couter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
