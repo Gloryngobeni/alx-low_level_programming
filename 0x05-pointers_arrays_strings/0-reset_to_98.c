499eac22ed20948dcc4cea7ed22536bb2e1588d0
Gloryngobeni <www.gloryngobeni@gmail.com>
2023-05-02 17:13:38 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 -a function that takes an int as parameter
+ * and updates the value it poibts to 98
+ * @n: input
+ * Return: n
+ */
+viod reset_to_98(int *n)
+{
+	*n = 98;
+}
